f1d25e192e45fc92a159e985c2c8ac953c5f288e
Shivanand.chachadi <shivu.2007@gmail.com>
2022-03-17 16:57:43 +0530

    added new method in main branch

---
+++ git.test/src/test/java/git/test/NewMethod.java
@@ -0,0 +1,13 @@
+package git.test;
+
+public class NewMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("new Method added here");
+		System.out.println("Hello new one more method added here");
+		
+	}
+
+}
