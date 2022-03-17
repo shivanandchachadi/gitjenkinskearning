ae1c5467bea6e8558fcfdd26344b88fd384c53e0
Shivanand.chachadi <shivu.2007@gmail.com>
2022-03-17 16:54:40 +0530

    added new features

--- git.test/src/test/java/git/test/Mytest.java
+++ git.test/src/test/java/git/test/Mytest.java
@@ -1,14 +1,23 @@
  package git.test;
 
+import org.junit.Test;
+
 public class Mytest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hi");
-System.out.println("hello");
-
-
-System.out.println("first release ");
-	}
+	
+	  public static void main(String[] args) { // TODO Auto-generated method stub
+	  
+		  System.out.println("fist release");
+		  System.out.println("Need to add more details");
+	  }
+	 
+	
+		/*
+		 * @Test public void run() { System.out.println("hi");
+		 * System.out.println("hello");
+		 * 
+		 * 
+		 * System.out.println("first release from test class"); }
+		 */
 
 }
---
+++ git.test/src/test/java/git/test/UtilityClass.java
@@ -0,0 +1,12 @@
+package git.test;
+
+public class UtilityClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mytest test=new Mytest();
+	//	test.main(args);
+		//test.run();
+	}
+
+}
